002cd970ae2de401127c4bf827db41eaceb59a0d
Marcelin <marcelinadedze88@gmail.com>
2024-01-26 23:03:16 +0000

    Add about page, images and styling with Tailwind

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -15,4 +15,12 @@ class PagesController extends AbstractController
     {
         return $this->render('pages/home.html.twig');
     }
+
+    /**
+     * @Route("/about-us", name="app_about")
+     */
+    public function about(): Response
+    {
+        return $this->render('pages/about.html.twig');
+    }
 }
