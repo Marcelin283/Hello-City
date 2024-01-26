2ca56269092c1fcd4142d0d621512be21490867f
Marcelin <marcelinadedze88@gmail.com>
2024-01-26 20:37:38 +0000

    Add twig and maker bundle, Add php.ini config, Setup PagesController and homepage

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function home(): Response
+    {
+        return $this->render('pages/home.html.twig');
+    }
+}
